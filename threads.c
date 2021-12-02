913226fe0bf62c57cfad5c658958afa88fe17fef
joshsj89 <joshsj89@gmail.com>
2021-12-01 21:19:22 -0800

    Create threads.c

---
+++ threads.c
@@ -0,0 +1,18 @@
+#include <pthread.h>
+#include <stdbool.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <strings.h>
+#include "lab10.h"
+
+#define NODE struct contact
+#define SIZE 26
+
+/*
+*****************************************************************
+*
+*       functions implementation
+*
+*****************************************************************
+*/
